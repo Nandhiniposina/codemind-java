859e9616a134aeba310a67b3854c251470322729
Nandhiniposina <106681730+Nandhiniposina@users.noreply.github.com>
2022-07-30 16:05:42 +0530

    import java.util.Scanner; class Sample {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int m,n,p;         m=sc.nextInt();         n=sc.nextInt();         p=m*n;         System.out.println(p);         sc.close();     } }

---
+++ Multiplication.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Sample
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int m,n,p;
+        m=sc.nextInt();
+        n=sc.nextInt();
+        p=m*n;
+        System.out.println(p);
+        sc.close();
+    }
+}
+    
