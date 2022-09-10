9d677439c9b336b655f6f3e007b0665fd606205b
Nandhiniposina <106681730+Nandhiniposina@users.noreply.github.com>
2022-09-10 19:46:18 +0530

    import java.util.Scanner; class main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,i,r;         n=sc.nextInt();         r=sc.nextInt();         for(i=1;i<=r;i=i+1)         {             if(i%2!=0)             {                 System.out.println(n+ " x " +i+ " = " +(n*i));             }         }     } }

---
+++ Tables.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,i,r;
+        n=sc.nextInt();
+        r=sc.nextInt();
+        for(i=1;i<=r;i=i+1)
+        {
+            if(i%2!=0)
+            {
+                System.out.println(n+ " x " +i+ " = " +(n*i));
+            }  
+        }
+    }
+}
