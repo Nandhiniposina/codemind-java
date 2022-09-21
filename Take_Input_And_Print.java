2e500bfb623ea8af36bababede8a3c4ad8c17da8
Nandhiniposina <106681730+Nandhiniposina@users.noreply.github.com>
2022-09-21 21:13:10 +0530

    import java.util.*; class main {     public static void main(String args[])     {         String s;         Scanner sc=new Scanner(System.in);         s=sc.nextLine();         System.out.println(s);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class main
+{ 
+    public static void main(String args[])
+    { 
+        String s;
+        Scanner sc=new Scanner(System.in);
+        s=sc.nextLine(); 
+        System.out.println(s); 
+    }
+}
