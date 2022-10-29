bbd48f31e113142ca09bd4f4280efbf7154a9d65
NANDHINI <106681730+Nandhiniposina@users.noreply.github.com>
2022-10-29 19:33:34 +0530

    import java.util.*; class main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,x[],sum=0;         n=sc.nextInt();         x=new int[n];         for(int i=0; i<n; i++)         {             x[i]=sc.nextInt();             }             for(int i=0; i<n; i++)             {                 double d=Math.sqrt(x[i]);                 if(d==(int)d)                 sum=sum+x[i];                 }                 System.out.println(sum);                 }
    }

---
+++ Program_to_find_the_sum_of_perfect_square_elements_in_an_array.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,x[],sum=0;
+        n=sc.nextInt();
+        x=new int[n];
+        for(int i=0; i<n; i++)
+        {
+            x[i]=sc.nextInt();
+            }
+            for(int i=0; i<n; i++) 
+            {
+                double d=Math.sqrt(x[i]); 
+                if(d==(int)d)
+                sum=sum+x[i];
+                } 
+                System.out.println(sum);
+                }
+    
+}
